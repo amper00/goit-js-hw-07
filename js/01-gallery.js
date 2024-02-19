f1032fa9a2f92c0d6f8867ead1d05aa4ff84ca52
Daniel Szul <amper00@gmail.com>
2024-02-19 01:15:58 +0100

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+const newArray = galleryItems.map(
+  (element) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${element.original}">
+      <img
+        class="gallery__image"
+        src="${element.preview}" 
+        alt="${element.description}"
+        data-source="${element.original}"
+        >
+        </img>
+        </a>
+     </li>`
+);
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+  
+    const lightbox = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+  
+    lightbox.show();
+  });
+  
+
+gallery.insertAdjacentHTML("afterbegin", newArray.join(``));
 console.log(galleryItems);
+
